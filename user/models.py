02eab53af7eb8de8b22022633465b29915ab6a8d
GaganDureja <gagandureja675@gmail.com>
2024-02-14 15:22:15 +0530

    homepage base done

--- user/models.py
+++ user/models.py
@@ -9,3 +9,16 @@ class User(AbstractUser):
     phone_verified = models.BooleanField(default=False)
     email_otp = models.CharField(max_length=25, null=True, blank=True )
     email_verified = models.BooleanField(default=False)
+    verified_account = models.BooleanField(default=False)
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    msg = models.CharField(max_length=500, null=True)
+
+    TWEET_TYPES = (
+        (0, 'Message'),
+        (1, 'Media'),
+        (2, 'Poll')
+    )
+    tweet_type = models.IntegerField(default=0, choices=TWEET_TYPES)
